7ab312ad5b98dd276bed447fb14d5254bcbdb0ea
Kl3va <ikwunzekelvin@gmail.com>
2023-04-04 01:04:02 +0100

    feat: added mock data to productSlice n verified on the product page

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from 'store/hooks'
 
 type Props = {}
 
 const Product = (props: Props) => {
   const { id } = useParams()
+  const { products } = useAppSelector((state) => state.products)
 
-  return <div>Product-{id}</div>
+  return (
+    <div>
+      Product-{id} // <span>{products[5].slug}</span>
+    </div>
+  )
 }
 
 export default Product
