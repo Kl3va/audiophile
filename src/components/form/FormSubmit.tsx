c777f9a65a3ab1483ca0f4730bcd67e5ab396edd
Kl3va <ikwunzekelvin@gmail.com>
2023-04-27 15:19:30 +0100

    feat: Added the form and summary component for the checkout page

---
+++ src/components/form/FormSubmit.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const FormSubmit = (props: Props) => {
+  return <section>FormSubmit</section>
+}
+
+export default FormSubmit
--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+//Go back button component
+import GoBack from 'components/go-back/GoBack'
+
+//FormSubmit component
+import FormSubmit from 'components/form/FormSubmit'
+
+//Summary component
+import Summary from 'components/summary/Summary'
+
 type Props = {}
 
 const Checkout = (props: Props) => {
-  return <main>Checkout</main>
+  return (
+    <main>
+      <GoBack />
+      <FormSubmit />
+      <Summary />
+    </main>
+  )
 }
 
 export default Checkout
